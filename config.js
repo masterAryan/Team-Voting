d0eb37638f8c0a899ec89a6b649a983c2c4cfba9
masterAryan <66153461+masterAryan@users.noreply.github.com>
2021-11-04 15:03:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,14 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+apiKey: "AIzaSyBSQBhRofXuLbVk10CGVjKP9EHHUY_hsQA",
+  authDomain: "team-voting-997cd.firebaseapp.com",
+  databaseURL: "https://team-voting-997cd-default-rtdb.firebaseio.com",
+  projectId: "team-voting-997cd",
+  storageBucket: "team-voting-997cd.appspot.com",
+  messagingSenderId: "343054300699",
+  appId: "1:343054300699:web:5599404d2fd1e3a623aabe"
+ 
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
